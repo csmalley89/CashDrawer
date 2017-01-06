0b25a4e5ad84db7d09d16a963cccb9ad7161c730
Chris Smalley <chrism.smalley@gmail.com>
2017-01-05 20:32:15 -0600

    added cash recieved screen Having issues with math when adding different items

--- app/controllers/OrderCtrl.js
+++ app/controllers/OrderCtrl.js
@@ -3,6 +3,8 @@
 app.controller("OrderCtrl", function($scope, InventoryFactory, $location){
   $scope.currentOrder = {};
   $scope.products = [];
+  $scope.showPayment = false;
+  $scope.hideTrans = false;
 
   $scope.addItemToOrder = function (product){
     console.log("product", product);
@@ -12,24 +14,35 @@ app.controller("OrderCtrl", function($scope, InventoryFactory, $location){
     }
     console.log("currentOrder", $scope.currentOrder);
     $scope.lineTotal(product);
+    $scope.orderTax(product);
+    $scope.orderSubtotal(product);
+    $scope.orderTotal(product);
   };
 
- $scope.lineTotal = (product) => {
+  $scope.lineTotal = (product) => {
     $scope.lineSubtotal = (product.price * product.purchaseNumber);
- };
+  };
 
+  $scope.orderTax = (product) => {
+    $scope.transactionTax = (product.taxRate * product.price);
+  };
 
+  $scope.orderSubtotal = (product) => {
+    $scope.transactionSubtotal = ($scope.lineSubtotal + $scope.transactionTax);
+  };
 
-  // $scope.addItemToOrder = function (product){
-  //   if ($scope.product.quantity >= 1) {
-  //     return;
-  //   }
-  //   var clickedItem = currentOrder[product.productId];
-  //   clickedItem++;
-  //   console.log("clickedItem", clickedItem);
-  //   console.log("currentOrder", currentOrder);
+  $scope.orderTotal = (product) => {
+    $scope.transactionTotal = ($scope.transactionSubtotal + $scope.transactionTax);
+  };
 
-  // };
+  $scope.payment = () => {
+    $scope.showPayment = true;
+    $scope.hideTrans = true;
+  };
+
+  $scope.changeDue = () => {
+    $scope.orderChange = ($scope.currentOrder.CashRecieved - $scope.transactionTotal);
+  };
 
   InventoryFactory.getProductList()
   .then(function(productCollection) {

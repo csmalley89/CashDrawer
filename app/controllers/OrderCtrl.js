fd9c8ad0605f7eb8effbd6b8ad8a85dd30eb61cf
Chris Smalley <chrism.smalley@gmail.com>
2017-01-06 13:22:31 -0600

    fixed cash recieved error

--- app/controllers/OrderCtrl.js
+++ app/controllers/OrderCtrl.js
@@ -31,21 +31,13 @@ app.controller("OrderCtrl", function($scope, InventoryFactory, $location){
     $scope.lineSubtotal = (product.price * product.purchaseNumber);
   };
 
-  // $scope.orderTax = (product) => {
-  //   $scope.transactionTax = (product.taxRate * product.price);
-  // };
-
-  // $scope.orderTotal = (product) => {
-  //   $scope.transactionTotal = ($scope.transactionSubtotal + $scope.transactionTax);
-  // };
-
   $scope.payment = () => {
     $scope.showPayment = true;
     $scope.hideTrans = true;
   };
 
   $scope.changeDue = () => {
-    $scope.orderChange = ($scope.currentOrder.CashRecieved - $scope.transactionTotal);
+    $scope.orderChange = ($scope.currentOrder.CashRecieved - $scope.orderTotal);
   };
 
   InventoryFactory.getProductList()
@@ -58,65 +50,3 @@ app.controller("OrderCtrl", function($scope, InventoryFactory, $location){
 });
 
 
-// app.controller("OrderCtrl", function($scope, InventoryFactory, $location){
-//   $scope.currentOrder = {};
-//   $scope.products = [];
-//   $scope.showPayment = false;
-//   $scope.hideTrans = false;
-
-//   $scope.addItemToOrder = function (product){
-//     console.log("product", product);
-//     if (!product.purchaseNumber || product.quantity > product.purchaseNumber) {
-//       product.purchaseNumber = product.purchaseNumber ? product.purchaseNumber += 1 : 1;
-//       $scope.currentOrder[product.productId]=product;
-//     }
-//     console.log("currentOrder", $scope.currentOrder);
-//     console.log("currentOrderPrice", $scope.currentOrder);
-//     $scope.lineTotal(product);
-//     $scope.orderTax(product);
-//     // $scope.orderSubtotal(product);
-//     $scope.orderTotal(product);
-//   };
-
-
-//   $scope.lineTotal = (product) => {
-//     $scope.lineSubtotal = (product.price * product.purchaseNumber);
-//   };
-
-
-//   $scope.orderTax = (product) => {
-//     $scope.transactionTax = (product.taxRate * product.price);
-//   };
-
-
-  // $scope.transactionSubtotal = (product) => {
-  //   var total = 0;
-  //   for(var i = 0; i < $scope.lineSubtotal.length; i++){
-  //     total += ($scope.lineSubtotal[i]);
-  //   }
-  //   return total;
-  // };
-
-
-
-//   $scope.orderTotal = (product) => {
-//     $scope.transactionTotal = ($scope.transactionSubtotal + $scope.transactionTax);
-//   };
-
-//   $scope.payment = () => {
-//     $scope.showPayment = true;
-//     $scope.hideTrans = true;
-//   };
-
-//   $scope.changeDue = () => {
-//     $scope.orderChange = ($scope.currentOrder.CashRecieved - $scope.transactionTotal);
-//   };
-
-//   InventoryFactory.getProductList()
-//   .then(function(productCollection) {
-//     $scope.products = productCollection;
-//   });
-
-
-
-// });
